ab48e1fe2b1a3dd59a718cf3d26c854b0fc71621
AsepNurdinDev <asep@student.undiksha.ac.id>
2025-05-03 15:33:16 +0800

    update: ganti path gambar dan konten About.jsx 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="text-center">
           <div className="w-80 h-80 mx-auto rounded-full overflow-hidden mb-8 border-4 border-blue-500">
             <img
-              src="/public/img/dsc_3884.jpg" 
+              src="/img/dsc_3884.jpg" 
               alt="Your Name"
               className="object-cover w-full h-full"
             />
